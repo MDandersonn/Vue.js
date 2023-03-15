277a215474812d29f374e23498b32c4c81c045cf
MDandersonn <kimyj1215@naver.com>
2023-03-16 02:58:57 +0900

    feat: [김영진] Vue3 아프리카도서관 axios설치 백엔드와 연결 [africa01]

--- africa/src/pages/Home.vue
+++ africa/src/pages/Home.vue
@@ -18,8 +18,10 @@
     <div class="container">
 
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-            <div class="col" v-for="i in 12" :key="i">
-                <Card/>
+            <div class="col" v-for="(item1, idx) in state.items" :key="idx">
+            <!-- {{item1}} -->
+                <!-- <Card/> -->
+                <Card :item="item1"/>
         
 
              </div>
@@ -32,11 +34,23 @@
 
 <script>
 import Card from '../components/Card.vue'
-
+import axios from "axios";
+import {reactive} from "vue";
 
 export default {
   components: { Card },
-    name: "Home"   
+    name: "Home"   ,
+    setup(){
+        const state = reactive({
+            items:[]
+        })
+        axios.get("/api/items")
+        .then( ({data})=>{    //res      ->res.data 대신  바로 data를 뽑아올수있다.
+            // console.log(res);
+            state.items = data;//배열 받아옴 [알파,베타,감마]
+        })
+        return {state}
+    }
 }
 </script>
 
--- africa/vue.config.js
+++ africa/vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports ={
+  devServer:{
+    proxy:{
+      '/api':{//api로시작하면 여기로 요청하겟다는의미
+        target: 'http://localhost:8080',
+        changeOrigin:true,
+        pathRewrite:{
+          '^/':''
+        }
+      }
+    }
+  }
+}
