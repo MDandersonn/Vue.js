ddf7b430bc1df498a57fda48953ca8937952f4e2
MDandersonn <kimyj1215@naver.com>
2023-03-20 01:50:45 +0900

    feat: [김영진] 아프리카도서관 장바구니,주문서 구현 [VUE3-AFRICA-04]

--- africa/src/scripts/router.js
+++ africa/src/scripts/router.js
@@ -1,13 +1,17 @@
 import {createRouter, createWebHistory} from 'vue-router';
-import Home from '../pages/Home.vue'
-import Login from '../pages/Login.vue'
-
+import Home from '../pages/Home.vue';
+import Login from '../pages/Login.vue';
+import Cart from '../pages/Cart.vue';
+import Order from '../pages/Order.vue';
 
 const routes = [
     {path:'/', component:Home},
-    {path:'/login', component:Login}
+    {path:'/login', component:Login},
+    {path:'/cart', component:Cart},
+    {path:'/order', component: Order}
 
 ]
+
 const router= createRouter({
     history:createWebHistory(),
     routes:routes
