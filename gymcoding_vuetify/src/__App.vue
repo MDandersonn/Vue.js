af3c0966e2a0486208f7ad9dcb4df00c69b31721
MDandersonn <kimyj1215@naver.com>
2023-03-21 03:22:12 +0900

    feat: [김영진] 뷰티파이 레이아웃 연습 [VUETIFY2-GYMCODING-02]

---
+++ gymcoding_vuetify/src/__App.vue
@@ -0,0 +1,27 @@
+<template>
+<!-- v-app은 반드시 최상단에 선언 -->
+<v-app>
+<!-- v-main :컨텐츠영역 -->
+  <v-main>
+  <!--  -->
+    <v-app-bar app/>
+    <v-navigation-drawer app/>
+    <v-container fluid>
+      컨텐츠영역
+    </v-container>
+  </v-main>
+  <v-footer app />
+  
+</v-app>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
