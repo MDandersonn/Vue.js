c7cd64ece8a58342a24e02a99acfbec107b90488
MDandersonn <kimyj1215@naver.com>
2023-03-21 03:22:43 +0900

    feat: [김영진] 뷰티파이 라우터 설정 [VUETIFY2-GYMCODING]

--- gymcoding_vuetify/src/router/index.js
+++ gymcoding_vuetify/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Dashboard from '../views/Dashboard.vue'
 import GridSystem from '../views/GridSystem.vue'
+import GridListPage from '../views/GridListPage.vue'
+import Breakpoints from '../views/Breakpoints.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +17,16 @@ const routes = [
     path: '/grid-system',
     name: 'GridSystem',
     component: GridSystem
+  },
+  {
+    path: '/grid-list-page',
+    name: 'GridListPage',
+    component: GridListPage
+  },
+  {
+    path: '/breakpoints',
+    name: 'Breakpoints',
+    component: Breakpoints
   },
 
 
