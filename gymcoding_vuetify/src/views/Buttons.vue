ab2f249c8d59b29456caa2e28823f534756eba6c
MDandersonn <kimyj1215@naver.com>
2023-03-21 05:36:52 +0900

    feat: [김영진] 뷰티파이 버튼 연습 [VUETIFY2-GYMCODING-13]

---
+++ gymcoding_vuetify/src/views/Buttons.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <v-card class="pa-3">
+      <v-row>
+        <v-col cols="auto">
+            <v-btn color="primary">버튼 </v-btn>
+        </v-col>
+        <v-col cols="auto">
+            <v-btn color="secondary">버튼 </v-btn>
+        </v-col>
+        <v-col cols="auto">
+            <v-btn color="success">버튼 </v-btn>
+        </v-col>
+        <v-col>
+            <v-btn color="error">버튼 </v-btn>
+        </v-col>
+      </v-row>
+    
+    </v-card>
+    
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
