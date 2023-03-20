6e736ac94f3f8dae3c4e51ec4c4322d8272dccda
MDandersonn <kimyj1215@naver.com>
2023-03-21 03:24:10 +0900

    feat: [김영진] 뷰티파이  연습 [VUETIFY2-GYMCODING-04]

--- gymcoding_vuetify/src/views/Dashboard.vue
+++ gymcoding_vuetify/src/views/Dashboard.vue
@@ -1,7 +1,10 @@
 <template>
-<div>
-  dashboard
-</div>
+<v-container class="red" fluid>
+  <!-- 늘리고싶다면fluid -->
+    <v-card class="yellow pa-3">
+      Dashboard
+    </v-card>
+  </v-container>
   
 </template>
 
