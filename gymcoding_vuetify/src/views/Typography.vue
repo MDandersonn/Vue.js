a3291fa7b1442a839fae9099f33c35b293209fa9
MDandersonn <kimyj1215@naver.com>
2023-03-21 04:22:07 +0900

    feat: [김영진] 뷰티파이 폰트색상 배경색상 연습 [VUETIFY2-GYMCODING-07]

--- gymcoding_vuetify/src/views/Typography.vue
+++ gymcoding_vuetify/src/views/Typography.vue
@@ -1,97 +1,68 @@
 <template>
   <v-container>
-  <v-card>
-    <v-card-title>
-        Typography
-    </v-card-title>
-    <v-contain class="grey lighten-3">
+    <v-card>
+      <v-card-title> Typography </v-card-title>
+      <v-contain class="grey lighten-3">
         <v-card class="pa-3" outlined>
-        <p class="text-h1">Heading1 </p>
-        <p class="text-h2">Heading2 </p>
-        <!-- sm이상일때는 subtitle1으로변경 -->
-        <p class="text-h3 text-sm-subtitle-1" >Heading3 </p>
+          <p class="text-h1">Heading1</p>
+          <p class="text-h2">Heading2</p>
+          <!-- sm이상일때는 subtitle1으로변경 -->
+          <p class="text-h3 text-sm-subtitle-1">Heading3</p>
 
+          <p class="font-weight-black">Black text.</p>
+          <p class="font-weight-bold">Bold text.</p>
+          <p class="font-weight-medium">Medium weight text.</p>
+          <p class="font-weight-regular">Normal weight text.</p>
+          <p class="font-weight-light">Light weight text.</p>
+          <p class="font-weight-thin">Thin weight text.</p>
+          <p class="font-italic">Italic text.</p>
+            <p class="text-justify">
+              Morbi mattis ullamcorper velit. Donec orci lectus, aliquam ut,
+              faucibus non, euismod id, nulla. Fusce convallis metus id felis
+              luctus adipiscing. Aenean massa. Vestibulum purus quam,
+              scelerisque ut, mollis sed, nonummy id, metus. Nulla consequat
+              massa quis enim. Praesent venenatis metus at tortor pulvinar
+              varius. Donec venenatis vulputate lorem. Phasellus accumsan cursus
+              velit. Pellentesque ut neque.
+            </p>
+          </v-card>
+          </v-contain></v-card>
+          <!-- 배경색변경 -->
+              <p class="text-left primary">
+              <!-- primary:배경 파랑색 -->
+                Left aligned text on all viewport sizes.
+                </p>
+                <p class="text-center secondary lighten-5">
+                <!-- secondary: 회색 -->
+                <!-- lighten-1 :여리게 -->
+                Center aligned text on all viewport sizes.
+                </p>
+                 <!-- darken-1 :어둡게 -->
+                <p class="text-right error darken-2">
+                <!-- error:레드 -->
+                Right aligned text on all viewport sizes.
+                </p>
+    <!-- 글자색 변경 -->
+                <p class="text-sm-left primary--text">
+                Left aligned text on viewports sized SM (small) or wider.
+                </p>
+                <p class="text-md-left">
+                Left aligned text on viewports sized MD (medium) or wider.
+                </p>
+                <p class="text-lg-left">
+                Left aligned text on viewports sized LG (large) or wider.
+                </p>
+                <p class="text-xl-left">
+                Left aligned text on viewports sized XL (extra-large) or wider.
+                </p>
+          
+          </v-container>
 
-                <!-- Heading 1
-                Heading 2
-                Heading 3
-                Heading 4
-                Heading 5
-                Heading 6
-                Subtitle 1
-                Subtitle 2
-                Body 1
-                Body 2
-                BUTTON
-                Caption
-                OVERLINE
-         -->
-
-    <p class="font-weight-black">
-      Black text.
-    </p>
-    <p class="font-weight-bold">
-      Bold text.
-    </p>
-    <p class="font-weight-medium">
-      Medium weight text.
-    </p>
-    <p class="font-weight-regular">
-      Normal weight text.
-    </p>
-    <p class="font-weight-light">
-      Light weight text.
-    </p>
-    <p class="font-weight-thin">
-      Thin weight text.
-    </p>
-    <p class="font-italic">
-      Italic text.
-    </p>
-
-    <template>
-  <p class="text-justify">
-    Morbi mattis ullamcorper velit. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Fusce convallis metus id felis luctus adipiscing. Aenean massa. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus.
-
-    Nulla consequat massa quis enim. Praesent venenatis metus at tortor pulvinar varius. Donec venenatis vulputate lorem. Phasellus accumsan cursus velit. Pellentesque ut neque.
-  </p>
-</template>
-
- <p class="text-left">
-      Left aligned text on all viewport sizes.
-    </p>
-    <p class="text-center">
-      Center aligned text on all viewport sizes.
-    </p>
-    <p class="text-right">
-      Right aligned text on all viewport sizes.
-    </p>
-
-    <p class="text-sm-left">
-      Left aligned text on viewports sized SM (small) or wider.
-    </p>
-    <p class="text-md-left">
-      Left aligned text on viewports sized MD (medium) or wider.
-    </p>
-    <p class="text-lg-left">
-      Left aligned text on viewports sized LG (large) or wider.
-    </p>
-    <p class="text-xl-left">
-      Left aligned text on viewports sized XL (extra-large) or wider.
-    </p>
-        </v-card>
-    </v-contain>
-  </v-card>
-
-  </v-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
