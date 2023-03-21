3d00ddcb3334a8a357f3c7924bd835f0de3bb0e2
MDandersonn <kimyj1215@naver.com>
2023-03-22 05:19:03 +0900

    feat: [김영진] vuetify공식홈페이지 툴바 앱바 연습 [VUETIFY-OFFICIAL-03]

---
+++ official_vuetify/src/views/03_appBarsView.vue
@@ -0,0 +1,134 @@
+
+<template>
+  <v-card class="overflow-hidden">
+    <v-app-bar
+      absolute
+      color="teal lighten-3"
+      dark
+      hide-on-scroll
+      scroll-target="#scrolling-techniques-4"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-sheet
+      id="scrolling-techniques-4"
+      class="overflow-y-auto"
+      max-height="600"
+    >
+      <v-container style="height: 1000px"></v-container>
+    </v-sheet>
+
+    <!-- 툴바 -->
+    <div>
+      <v-toolbar
+        dark
+        src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+      >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Vuetify</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-export</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </div>
+
+    <!-- 버티컬 툴바 -->
+    <v-toolbar color="purple" dark>
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-divider class="mx-4" vertical></v-divider>
+
+      <span class="subheading">My Home</span>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn text> News </v-btn>
+
+        <v-divider vertical></v-divider>
+
+        <v-btn text> Blog </v-btn>
+
+        <v-divider vertical></v-divider>
+
+        <v-btn text> Music </v-btn>
+
+        <v-divider vertical></v-divider>
+      </v-toolbar-items>
+
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </v-toolbar>
+
+    <!-- 푸터 -->
+
+    <v-footer dark padless>
+      <v-card flat tile class="indigo lighten-1 white--text text-center">
+        <v-card-text>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4 white--text"
+            icon
+          >
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-text>
+
+        <v-card-text class="white--text pt-0">
+          Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet.
+          Mauris cursus commodo interdum. Praesent ut risus eget metus luctus
+          accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim
+          a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula
+          lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus
+          iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum
+          tempor vel ut orci. Orci varius natoque penatibus et magnis dis
+          parturient montes, nascetur ridiculus mus.
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+  </v-card>
+</template>
+<script>
+export default {
+  data: () => ({
+    collapseOnScroll: true,
+      icons: [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ],
+  }),
+};
+</script>
+
+<style>
+</style>
