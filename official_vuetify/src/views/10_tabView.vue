b403fdada2d4f12b484f41fcf45c44a60b969567
MDandersonn <kimyj1215@naver.com>
2023-03-24 03:46:33 +0900

    feat: [김영진] vuetify공식홈페이지 tab연습 [VUETIFY-OFFICIAL-10]

---
+++ official_vuetify/src/views/10_tabView.vue
@@ -0,0 +1,402 @@
+<template>
+  <div>
+    <!-- 탭 -->
+    <v-tabs>
+      <v-tab>Item One</v-tab>
+      <v-tab>Item Two</v-tab>
+      <v-tab>Item Three</v-tab>
+    </v-tabs>
+
+    <!-- 탭2 -->
+    <v-card>
+      <v-toolbar color="cyan" dark flat>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Your Dashboard</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+
+        <template v-slot:extension>
+          <v-tabs v-model="tab" align-with-title>
+            <v-tabs-slider color="yellow"></v-tabs-slider>
+
+            <v-tab v-for="item in items" :key="item">
+              {{ item }}
+            </v-tab>
+          </v-tabs>
+        </template>
+      </v-toolbar>
+
+      <v-tabs-items v-model="tab">
+        <v-tab-item v-for="item in items" :key="item">
+          <v-card flat>
+            <v-card-text v-text="text"></v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
+    <br><br>
+
+    <!-- 옆으로 게속 페이지네이션되는 탭 -->
+      <v-card>
+    <v-tabs
+      background-color="deep-purple accent-4"
+      center-active
+      dark
+    >
+      <v-tab>One</v-tab>
+      <v-tab>Two</v-tab>
+      <v-tab>Three</v-tab>
+      <v-tab>Four</v-tab>
+      <v-tab>Five</v-tab>
+      <v-tab>Six</v-tab>
+      <v-tab>Seven</v-tab>
+      <v-tab>Eight</v-tab>
+      <v-tab>Nine</v-tab>
+      <v-tab>Ten</v-tab>
+      <v-tab>Eleven</v-tab>
+      <v-tab>Twelve</v-tab>
+      <v-tab>Thirteen</v-tab>
+      <v-tab>Fourteen</v-tab>
+      <v-tab>Fifteen</v-tab>
+      <v-tab>Sixteen</v-tab>
+      <v-tab>Seventeen</v-tab>
+      <v-tab>Eighteen</v-tab>
+      <v-tab>Nineteen</v-tab>
+      <v-tab>Twenty</v-tab>
+    </v-tabs>
+  </v-card>
+<br><br>
+  <!-- 고정탭 -->
+   <v-tabs
+    fixed-tabs
+    background-color="indigo"
+    dark
+  >
+    <v-tab>
+      Option
+    </v-tab>
+    <v-tab>
+      Another Selection
+    </v-tab>
+    <v-tab>
+      Items
+    </v-tab>
+    <v-tab>
+      Another Screen
+    </v-tab>
+  </v-tabs>
+  <br><br>
+
+  <!-- 예시 -->
+   <v-card color="basil">
+    <v-card-title class="text-center justify-center py-6">
+      <h1 class="font-weight-bold text-h2 basil--text">
+        BASiL
+      </h1>
+    </v-card-title>
+
+    <v-tabs
+      v-model="tab1"
+      background-color="transparent"
+      color="basil"
+      grow
+    >
+      <v-tab
+        v-for="item in items1"
+        :key="item"
+      >
+        {{ item }}
+      </v-tab>
+    </v-tabs>
+
+    <v-tabs-items v-model="tab1">
+      <v-tab-item
+        v-for="item in items1"
+        :key="item"
+      >
+        <v-card
+          color="basil"
+          flat
+        >
+          <v-card-text>{{ text1 }}</v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-card>
+
+  <!-- 예시2 -->
+   <v-card>
+    <v-tabs
+      v-model="tab2"
+      background-color="deep-purple accent-4"
+      centered
+      dark
+      icons-and-text
+    >
+      <v-tabs-slider></v-tabs-slider>
+
+      <v-tab href="#tab-1">
+        Recents
+        <v-icon>mdi-phone</v-icon>
+      </v-tab>
+
+      <v-tab href="#tab-2">
+        Favorites
+        <v-icon>mdi-heart</v-icon>
+      </v-tab>
+
+      <v-tab href="#tab-3">
+        Nearby
+        <v-icon>mdi-account-box</v-icon>
+      </v-tab>
+    </v-tabs>
+
+    <v-tabs-items v-model="tab2">
+      <v-tab-item
+        v-for="i in 3"
+        :key="i"
+        :value="'tab-' + i"
+      >
+        <v-card flat>
+          <v-card-text>{{ text2 }}</v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-card>
+<br><br>
+  <!-- 예시3 -->
+   <v-card>
+    <v-tabs
+      color="deep-purple accent-4"
+      right
+    >
+      <v-tab>Landscape</v-tab>
+      <v-tab>City</v-tab>
+      <v-tab>Abstract</v-tab>
+
+      <v-tab-item
+        v-for="n in 3"
+        :key="n"
+      >
+        <v-container fluid>
+          <v-row>
+            <v-col
+              v-for="i in 6"
+              :key="i"
+              cols="12"
+              md="4"
+            >
+              <v-img
+                :src="`https://picsum.photos/500/300?image=${i * n * 5 + 10}`"
+                :lazy-src="`https://picsum.photos/10/6?image=${i * n * 5 + 10}`"
+                aspect-ratio="1"
+              ></v-img>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-tab-item>
+    </v-tabs>
+  </v-card>
+<br><br>
+
+<!-- 예시4 -->
+ <v-card>
+    <v-toolbar
+      color="cyan"
+      dark
+      flat
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Page title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+
+      <template v-slot:extension>
+        <v-tabs
+          v-model="model"
+          centered
+          slider-color="yellow"
+        >
+          <v-tab
+            v-for="i in 3"
+            :key="i"
+            :href="`#tab-${i}`"
+          >
+            Item {{ i }}
+          </v-tab>
+        </v-tabs>
+      </template>
+    </v-toolbar>
+
+    <v-tabs-items v-model="model">
+      <v-tab-item
+        v-for="i in 3"
+        :key="i"
+        :value="`tab-${i}`"
+      >
+        <v-card flat>
+          <v-card-text v-text="text3"></v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-card>
+  <br><br>
+
+
+  <!--  압축메뉴포함된 예시-->
+  <v-card>
+    <v-toolbar
+      color="deep-purple accent-4"
+      dark
+      flat
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Page title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+
+      <template v-slot:extension>
+        <v-tabs
+          v-model="currentItem"
+          fixed-tabs
+          slider-color="white"
+        >
+          <v-tab
+            v-for="item in items4"
+            :key="item"
+            :href="'#tab-' + item"
+          >
+            {{ item }}
+          </v-tab>
+
+          <v-menu
+            v-if="more.length"
+            bottom
+            left
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                text
+                class="align-self-center mr-4"
+                v-bind="attrs"
+                v-on="on"
+              >
+                more
+                <v-icon right>
+                  mdi-menu-down
+                </v-icon>
+              </v-btn>
+            </template>
+
+            <v-list class="grey lighten-3">
+              <v-list-item
+                v-for="item in more"
+                :key="item"
+                @click="addItem(item)"
+              >
+                {{ item }}
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-tabs>
+      </template>
+    </v-toolbar>
+
+    <v-tabs-items v-model="currentItem">
+      <v-tab-item
+        v-for="item in items.concat(more)"
+        :key="item"
+        :value="'tab-' + item"
+      >
+        <v-card flat>
+          <v-card-text>
+            <h2>{{ item }}</h2>
+            {{ text4 }}
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-card>
+
+
+
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+         currentItem: 'tab-Web',
+      items4: [
+        'Web', 'Shopping', 'Videos', 'Images',
+      ],
+      more: [
+        'News', 'Maps', 'Books', 'Flights', 'Apps',
+      ],
+      text4: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+         model: 'tab-2',
+        text3: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      
+        tab2: null,
+        text2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+     
+        tab1: null,
+        items1: [
+          'Appetizers', 'Entrees', 'Deserts', 'Cocktails',
+        ],
+        text1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      
+      tab: null,
+      items: ["web", "shopping", "videos", "images", "news"],
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    };
+  },
+
+  methods: {
+      addItem (item) {
+        const removed = this.items.splice(0, 1)
+        this.items.push(
+          ...this.more.splice(this.more.indexOf(item), 1),
+        )
+        this.more.push(...removed)
+        this.$nextTick(() => { this.currentItem = 'tab-' + item })
+      },
+  }
+}
+</script>
+
+<style scoped>
+.basil {
+  background-color: #FFFBE6 !important;
+}
+.basil--text {
+  color: #356859 !important;
+}
+</style>
